2fe317ab86c30a63744ab164ce08a6b346a9bf7f
Harrison <hsookim90@gmail.com>
2021-02-08 13:56:37 -0600

    Separate a login-feedback modal from the homescreen.

--- resources/js/screens/HomeScreen.jsx
+++ resources/js/screens/HomeScreen.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 
 import Loader from '../components/utilities/Loader';
 import Message from '../components/utilities/Message';
 import Product from '../components/Product';
+import LoginFeedback from '../components/LoginFeedback';
 
 import { listProducts } from '../redux/actions/productActions';
 
 const HomeScreen = () => {
-  const [show, setShow] = useState(false);
-
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productListReducer);
@@ -19,9 +18,6 @@ const HomeScreen = () => {
   const loggedinUserInfo = useSelector((state) => state.userLoginReducer);
   const { userInfo } = loggedinUserInfo;
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
@@ -29,27 +25,7 @@ const HomeScreen = () => {
   return (
     <Container>
       {/* TODO: modal should pop up only when an user just entered their credential and redirected to this screeen. */}
-      {userInfo ? (
-        <>
-          <Button variant="primary" onClick={handleShow}>
-            Test a modal
-          </Button>
-
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Welcome back, {userInfo.username}!</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>You are successfully signed in!</Modal.Body>
-            <Modal.Footer>
-              <Button variant="primary" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      ) : (
-        ''
-      )}
+      {userInfo ? <LoginFeedback /> : ''}
 
       <Row>
         <Col>
